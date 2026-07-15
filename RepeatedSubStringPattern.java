3e354c52966fce8e2b5bc9e8d01c3b8e070628e1
tanve13 <171332553+tanve13@users.noreply.github.com>
2026-07-15 19:17:44 +0530

    commit

--- RepeatedSubStringPattern.java
+++ RepeatedSubStringPattern.java
@@ -19,7 +19,7 @@ public class RepeatedSubStringPattern {
             return (length > 0 && n % (n - length) == 0);
         }
     }
-    
+    // 
    public static void main(String[] args) {
         Solution sol = new Solution();
         System.out.println(sol.repeatedSubstringPattern("abab")); // true
